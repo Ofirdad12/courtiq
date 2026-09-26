cff2fa2723f55fc987f56f01ee4635446aa38b8e
Ofirdad12 <49944480+Ofirdad12@users.noreply.github.com>
2026-09-26 13:30:19 +0300

    Add Player Memory V2 trend API

--- data.js
+++ data.js
@@ -138,6 +138,7 @@
     return setCompetitions([...current.map(x=>x.competition),value]);
   }
 
+  async function playerMemoryV2(playerId,competition=null){return jsonFetch("/rest/v1/rpc/player_memory_v2",{method:"POST",body:JSON.stringify({p_player_id:Number(playerId),p_competition:competition||null})});}
   async function teamMemory(competition,team){return jsonFetch("/rest/v1/rpc/team_memory",{method:"POST",body:JSON.stringify({comp:competition,team})});}
   async function qualityChecks(gameId){return jsonFetch("/rest/v1/data_quality_checks?select=*&game_id=eq."+Number(gameId)+"&order=created_at.desc");}
   async function intelligenceClaims(competition,subjectName){let p="/rest/v1/intelligence_claims?select=*&competition=eq."+encodeURIComponent(competition)+"&order=confidence.desc.nullslast,created_at.desc";if(subjectName)p+="&subject_name=eq."+encodeURIComponent(subjectName);return jsonFetch(p);}
@@ -242,7 +243,7 @@
   }
 
   window.CourtIQData = {
-    createPilotAccount, requestPasswordReset, recoverySessionFromUrl, updatePassword, signIn, signOut, refreshSession, user, workspace, playerIntelligence, competitionAccess, setCompetitions, chooseCompetition, teamMemory, qualityChecks, intelligenceClaims, myProfile, comparisonPlayers, gameReports, importRuns, productHealth, importOfficialGame, coachWorkspace, saveScheduleGame, addEvidence, reviewReport, reportReviewStatus,
+    createPilotAccount, requestPasswordReset, recoverySessionFromUrl, updatePassword, signIn, signOut, refreshSession, user, workspace, playerIntelligence, competitionAccess, setCompetitions, chooseCompetition, playerMemoryV2, teamMemory, qualityChecks, intelligenceClaims, myProfile, comparisonPlayers, gameReports, importRuns, productHealth, importOfficialGame, coachWorkspace, saveScheduleGame, addEvidence, reviewReport, reportReviewStatus,
     isSignedIn: () => !!readSession()?.access_token
   };
 })();
